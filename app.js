4da68925b8215bbbba4d27b9bbf4116d7865c088
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-17 16:24:14 +0600

    completed create element part

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const taskButton = document.getElementById('task-added-btn');
 
 taskButton.addEventListener('click', function () {
-  const inputValue = document.getElementById('task-field').value;
-  displayTodo(inputValue);
+  displayTodo();
 });
 
 const displayTodo = () => {
   const list = document.getElementById('list');
   const inputValue = document.getElementById('task-field').value;
   const lists = document.createElement('div');
-  lists.innerHTML = `<div class="col-sm">${inputValue}</div>`;
+  lists.classList = 'col-md-3 card m-2 p-2';
+  lists.innerHTML = `${inputValue} ${today}`;
   list.appendChild(lists);
 };
